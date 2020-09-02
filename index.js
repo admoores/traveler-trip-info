eb4aad3aaaf2e86a7719418a43b05e724ff1eca9
Alex Moores <moores.alexd@gmail.com>
2020-09-02 00:09:44 -0400

    did the thing

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
-'use strict';
+"use strict";
 
-// TODO Import what you need
+import airlinesService from "api/airlines.service.js";
+import profilesService from "api/profiles.service.js";
+import tripsService from "api/trip.service.js";
 
 function getTravelersFlightInfo() {
+  function airlinesToObj(airlinesList) {
+    const retObj = {};
 
-  // TODO Replace this hard coded response with your code
+    airlinesList.forEach((airline) => {
+      retObj[airline.code] = airline;
+    });
 
-  return {
-    travelers: [
-      {
-        id: 1,
-        name: 'Neo',
-        flights: [
-          {
-            legs: [
-              {
-                airlineCode: 'AA',
-                airlineName: 'American',
-                flightNumber: 'AA456',
-                frequentFlyerNumber: ''
-              }
-            ]
-          },
-          {
-            legs: [
-              {
-                airlineCode: 'VA',
-                airlineName: 'Virgin',
-                flightNumber: 'VA789',
-                frequentFlyerNumber: 'NVA123'
-              },
-              {
-                airlineCode: 'AK',
-                airlineName: 'Alaskan',
-                flightNumber: 'AK789',
-                frequentFlyerNumber: 'NAK123'
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  };
+    return retObj;
+  }
+
+  async function createOutput() {
+    const airlines = airlinesToObj(await airlinesService.get());
+    const listOfTravelers = await profilesService.get();
+    const tripObj = await tripsService.get();
+
+    return listOfTravelers.map((traveler) => {
+      const travelerFlights = tripObj.trip.flights.filter((t) => {
+        return t.travelerIds.includes(traveler.personId);
+      });
+
+      const flightsList = travelerFlights.map((flight) => {
+        return flight.legs.map((leg) => {
+          return {
+            airlineCode: leg.airlineCode,
+            airlineName: airlines[leg.airlineCode],
+            flightNumber: leg.flightNumber,
+            frequentFlyerNumber:
+              traveler.rewardPrograms.air[leg.airlineCode] || ""
+          };
+        });
+      });
+
+      return {
+        id: traveler.id,
+        name: traveler.name,
+        flights: flightsList
+      };
+    });
+  }
+
+  return createOutput();
 }
 
 module.exports = getTravelersFlightInfo;
